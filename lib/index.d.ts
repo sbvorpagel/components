b6020695d9bedbb957cb837b60fc144c0d9fb630
sbvorpagel <sbvorpagel@gmail.com>
2019-09-09 23:32:47 -0300

    Create a 0.4.0 version.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
-import { IonApp as App, IonCard as Card, IonCardHeader as CardHeader, IonCardSubtitle as CardSubtitle, IonCardTitle as CardTitle, IonContent as Content, IonInput as Input, IonItem as Item, IonLabel as Label, IonList as List, IonText as Text } from '@ionic/react';
+import { IonApp as App, IonCard as Card, IonCardHeader as CardHeader, IonCardSubtitle as CardSubtitle, IonCardTitle as CardTitle, IonContent as Content, IonInput as Input, IonItem as Item, IonLabel as Label, IonList as List, IonText as Text, IonSkeletonText as Skeleton } from '@ionic/react';
 import CardNews from './cardnews/CardNews';
 import ListNews from './listnews/ListNews';
 import News from './news/News';
-export { App, Card, CardHeader, CardSubtitle, CardTitle, Content, Input, Item, Label, List, News, Text, CardNews, ListNews, };
+export { App, Card, CardHeader, CardSubtitle, CardTitle, Content, Input, Item, Label, List, Text, Skeleton, CardNews, ListNews, News, };
 //# sourceMappingURL=index.d.ts.map
